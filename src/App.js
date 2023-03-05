1410c0dd03f9669f0d0740c665ff423fae162333
chris <Chrispyohong@gmail.com>
2023-03-05 09:55:11 -0800

    changes made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SplashPage from './components/SplashPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SplashPage />
-    </div>
+    <BrowserRouter>
+
+        <div className="App">
+          <SplashPage />
+        </div>
+    </BrowserRouter>
   );
 }
 
