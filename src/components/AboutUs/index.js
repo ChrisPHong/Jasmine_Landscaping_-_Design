b183e76b780e0d99335757121ad66487e25e3817
chris <Chrispyohong@gmail.com>
2023-03-30 18:20:53 -0700

    fixed css on splash page

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,21 +1,30 @@
-import './AboutUs.css';
-import photo from './dad.jpg'
+import "./AboutUs.css";
+import photo from "./dad.jpg";
+import { useNavigate } from "react-router";
+
 
 const AboutUs = () => {
-
-    return (
-        <div className='About-Us-Container'>
-            <div className='image-container'>
-                <img className='selfie' src={photo}/>
-                <div>
-
-                <p className='AboutUs-p-tag'>Hello, my name is James and I have experience in a multitude of services when it comes to landscaping. I have designed frontyards, backyards, patios, and collaborated in creating a swimming pool. Please feel free to contact me anytime for an estimate on your frontyard, backyard, and any other landscaping feature you'd like. Contact me @ jamesSoonHong@gmail.com. Feel free to look through my work via instagram.
-                </p>
-                </div>
-            </div>
+const navigate = useNavigate();
+  return (
+    <div className="About-Us-Container">
+      <div className="image-container">
+        <p>Hello there! My name is James</p>
+        <img className="selfie" src={photo} />
+        <div>
+          <p className="AboutUs-p-tag">
+            Hello, my name is James and I have experience in a multitude of
+            services when it comes to landscaping. I have designed frontyards,
+            backyards, patios, and collaborated in creating a swimming pool.
+            Please feel free to contact me anytime for an estimate on your
+            frontyard, backyard, and any other landscaping feature you'd like.
+            Contact me @ jamesSoonHong@gmail.com. Feel free to look through my
+            work via instagram.
+          </p>
+          <button className="button-contact" onClick={()=>{navigate('/contact')}}>Contact Me</button>
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -1,24 +1,32 @@
-import { NavLink } from 'react-router-dom';
-import './SplashPage.css'
-import splashPhoto from './splashpage.jpg'
+import { useNavigate } from "react-router-dom";
+import "./SplashPage.css";
+import splashPhoto from "./splashpage.jpg";
 
 const SplashPage = () => {
-    return (
-        <>
-            <div className='full-container'>
-                <div>
-                    <img className='SplashPhoto' src={splashPhoto} />
-                    <h1 className='company-name'>Jasmine Landscaping & Design</h1>
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        className="full-container"
+        style={{ backgroundImage: `url(${splashPhoto})` }}
+      >
+        <div>
+          {/* <img className="SplashPhoto" src={splashPhoto} /> */}
+          <h1 className="company-name">Jasmine Landscaping & Design</h1>
 
-                    <NavLink to='/Contact'>Contact</NavLink>
-                    {/* <p className='company-name'>Jasmine Landscaping & Design</p> */}
-                </div>
-            </div>
-        </>
-
-    )
-}
-
-
+          <button
+            className="button-contact"
+            onClick={() => {
+              navigate("/Contact");
+            }}
+          >
+            Contact Us
+          </button>
+          {/* <p className='company-name'>Jasmine Landscaping & Design</p> */}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SplashPage;
