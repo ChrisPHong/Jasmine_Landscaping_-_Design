81c605bab4268ace0e05a06ac5bf75a7d1383bb1
chris <Chrispyohong@gmail.com>
2023-04-11 17:05:12 -0700

    added a modal

--- src/components/PagePhotos/index.js
+++ src/components/PagePhotos/index.js
@@ -2,11 +2,12 @@ import SinglePhoto from "../SinglePhoto"
 
 
 const PagePhotos = ({ photos, page }) => {
-
+    // console.log(photos, "<<<< ph[t")
     return (
         <>
 
             {photos.map((photo, idx) => {
+
                 return (
                     <div key={idx}>
 
--- src/components/SinglePhoto/index.js
+++ src/components/SinglePhoto/index.js
@@ -1,18 +1,47 @@
-import "./SinglePhoto.css"
-
-
-
+import { useState } from "react";
+import "./SinglePhoto.css";
 
 const SinglePhoto = (props) => {
-    return (
-        <div className="singlePhoto-div">
-        <img  className='photo' src= {props.photo} />
-        </div>
+  const [show, setShow] = useState(false);
 
-    )
-
-
-}
-
+  const toggleModal = () => {
+    setShow(!show);
+  };
+  if (show) {
+    document.body.classList.add("active-modl");
+  } else {
+    document.body.classList.remove("active-modl");
+  }
+  return (
+    <>
+      {show &&
+        ((
+          <div className="modal">
+            <div onClick={toggleModal} className="overlay"></div>
+            <div className="modal-content">
+              <img className="modal-picture" src={props.photo} />
+              <button
+                className="close-modal"
+                onClick={() => {
+                  toggleModal();
+                }}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ): null)}
+      <div className="singlePhoto-div">
+        <img
+          className="photo"
+          src={props.photo}
+          onClick={() => {
+            toggleModal();
+          }}
+        />
+      </div>
+    </>
+  );
+};
 
 export default SinglePhoto;
