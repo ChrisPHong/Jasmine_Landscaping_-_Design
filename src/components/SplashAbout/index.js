c5a3a6203e2e6d1d7e18be04d68707d7e7880a78
chris <Chrispyohong@gmail.com>
2023-05-05 15:53:15 -0700

    changed the splash page

--- src/components/SplashAbout/index.js
+++ src/components/SplashAbout/index.js
@@ -12,18 +12,10 @@ const SplashAbout = () => {
         <div className="AboutJLD-Info">
           <h3 className="AboutJLDh3">About JLD</h3>
           <p className="about-p-tag">
-            Jasmine landscape & design started as a landscape company in 1993.
-            Through the years in Landscape Design & build, amassed knowledge,
-            talented individuals, and partners that propelled the company into a
-            successful Landscape Company, and now adding building to the
-            portfolio. Jasmine landscape & design Build strives to bring indoor
-            living to the outdoors. We believe that the outdoors is an extension
-            of our homes and should not be neglected. Our company seeks to
-            tackle each project differently based on the lifestyle, taste and
-            preferences of our clients. We work together with the homeowner from
-            the beginning to the end. We help with the landscape design and
-            build, the HOA submission process, and material and plant
-            selection.
+            Jasmine Landscape & Design was established in 1993 and has since
+            built and designed indoor and outdoor live spaces. We have worked on
+            patios, pools, barbecues, and many more features for your frontyard
+            and backyard needs! Click the button to learn more about Jasmine Landscape & Design!
           </p>
           <button
             className="learnMoreButton"
